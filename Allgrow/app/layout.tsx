60f0fbf377c107e3b000b2484d0b0fb663b8c9f3
yatinsingh2007 <yatin4591@gmail.com>
2025-09-23 21:28:09 +0530

    Protected Route added

--- Allgrow/app/layout.tsx
+++ Allgrow/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ProtectContextProvider from "@/context/protect";
+import ProtectedRouteProvider from "@/context/ProtectedRoute";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -30,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
-        {children}
+        <ProtectedRouteProvider>
+          {children}
+        </ProtectedRouteProvider>
       </body>
     </html>
   );
