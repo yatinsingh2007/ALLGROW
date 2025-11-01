52e242813804cbcf31d65e8185095c0abd56d88a
yatinsingh2007 <yatin4591@gmail.com>
2025-11-01 19:55:17 +0530

    deployment error fix

--- vinticode-frontend/app/dashboard/profile/Solvedquestion 2/page.tsx
+++ vinticode-frontend/app/dashboard/profile/Solvedquestion 2/page.tsx
@@ -1,7 +1,10 @@
+"use client"
 export default function SolvedQuestion(){
     return (
         <>
-            
+            <h1>
+                This is a solved question component.
+            </h1>
         </>
     )
 }
