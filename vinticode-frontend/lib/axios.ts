07ff16a98138740c7710bb1771c10e2ab5e0f767
yatinsingh2007 <yatin4591@gmail.com>
2025-11-05 15:45:39 +0530

    axios.ts api error

--- vinticode-frontend/lib/axios.ts
+++ vinticode-frontend/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "https://vinti-code.vercel.app/api"
+    baseURL : "https://vinti-code.vercel.app/api" ,
+    withCredentials : true
 })
 
 export default api
