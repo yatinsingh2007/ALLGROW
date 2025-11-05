490dcebf7bedbcb7e46b722f9d20e2c0f25539e1
yatinsingh2007 <yatin4591@gmail.com>
2025-11-05 11:20:21 +0530

    backend url added

--- vinticode-frontend/lib/axios.ts
+++ vinticode-frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "http://localhost:7777/api" ,
+    baseURL : "https://vinti-code.vercel.app/api" ,
     withCredentials : true
 })
 
