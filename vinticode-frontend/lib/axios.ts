8fda64cdd34633b388f41bffe5d72c8512f053d5
yatinsingh2007 <yatin4591@gmail.com>
2025-11-13 10:20:01 +0530

    backend url encypted

--- vinticode-frontend/lib/axios.ts
+++ vinticode-frontend/lib/axios.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 const api = axios.create({
-    baseURL : `${process.env.BACKEND_URL}`,
+    baseURL : `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
     withCredentials : true
 })
 
